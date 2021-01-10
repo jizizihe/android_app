796c88f389f7ba537ec4c4d60ca1a02996ea26d7
jizizihe <jizizihe@163.com>
2021-01-10 20:14:47 +0800

    初级控件

---
+++ 2_primary_control/初级控件.mm
@@ -0,0 +1,135 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="初级控件" FOLDED="false" ID="ID_1510939434" CREATED="1607776654817" MODIFIED="1610279535030" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" background="#9cb6b8" zoom="1.61">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" STYLE="as_parent">
+<font SIZE="9"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" STYLE="as_parent" TEXT_ALIGN="LEFT">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="as_parent">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="as_parent">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" STYLE="as_parent">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" STYLE="as_parent">
+<icon BUILTIN="yes"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="as_parent">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" STYLE="as_parent">
+<font SIZE="16"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439" STYLE="as_parent">
+<font SIZE="14"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" STYLE="as_parent">
+<font SIZE="12"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" STYLE="as_parent">
+<font SIZE="10"/>
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" STYLE="as_parent">
+<edge STYLE="sharp_bezier" COLOR="#ff6666" WIDTH="2"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="屏幕显示" POSITION="right" ID="ID_1248706445" CREATED="1610279486350" MODIFIED="1610279492147">
+<edge COLOR="#ff0000"/>
+<node TEXT="像素" ID="ID_1963079516" CREATED="1610279617204" MODIFIED="1610279629644">
+<node TEXT="px(像素)，与设备有关" ID="ID_332288616" CREATED="1610279651192" MODIFIED="1610279781810"/>
+<node TEXT="in(英寸)" ID="ID_970062432" CREATED="1610279668142" MODIFIED="1610279683130"/>
+<node TEXT="dp(与设备无关的单位)，与屏幕尺寸有关" ID="ID_459585109" CREATED="1610279683516" MODIFIED="1610279725578"/>
+<node TEXT="sp(用于设置字体大小)" ID="ID_680946427" CREATED="1610279726054" MODIFIED="1610279747255"/>
+<node TEXT="density(像素密度)=px/dp" ID="ID_1072173389" CREATED="1610279835735" MODIFIED="1610279879827"/>
+</node>
+<node TEXT="颜色" ID="ID_1135302180" CREATED="1610279630190" MODIFIED="1610279636346">
+<node TEXT="八位编码(如#FFEEDDCC)" ID="ID_203342359" CREATED="1610279983937" MODIFIED="1610280342413"/>
+<node TEXT="六位编码(如#EEDDCC)" ID="ID_1040467453" CREATED="1610279997078" MODIFIED="1610280346227"/>
+</node>
+</node>
+<node TEXT="图形基础" POSITION="right" ID="ID_520439898" CREATED="1610279494260" MODIFIED="1610279589367">
+<edge COLOR="#0000ff"/>
+<node TEXT="图形Drawable" ID="ID_1405415840" CREATED="1610280637818" MODIFIED="1610280647623"/>
+<node TEXT="状态列条图形" ID="ID_88298601" CREATED="1610280647809" MODIFIED="1610280664313"/>
+<node TEXT="形状图形" ID="ID_468166101" CREATED="1610280664754" MODIFIED="1610280672806"/>
+<node TEXT="九宫格图片" ID="ID_1813688617" CREATED="1610280679517" MODIFIED="1610280685954"/>
+</node>
+<node TEXT="简单布局" POSITION="left" ID="ID_1719196466" CREATED="1610279522571" MODIFIED="1610279528473">
+<edge COLOR="#00ff00"/>
+<node TEXT="View基本视图" ID="ID_1958950417" CREATED="1610280411247" MODIFIED="1610280425979"/>
+<node TEXT="线性布局LinearLayout" ID="ID_1963849195" CREATED="1610280436579" MODIFIED="1610280456804"/>
+<node TEXT="滚动视图ScrollView" ID="ID_154626269" CREATED="1610280457122" MODIFIED="1610280487490"/>
+</node>
+<node TEXT="简单控件" POSITION="left" ID="ID_542591238" CREATED="1610279529043" MODIFIED="1610279563299">
+<edge COLOR="#ff00ff"/>
+<node TEXT="文本视图TextView" ID="ID_411806528" CREATED="1610280501733" MODIFIED="1610280519119"/>
+<node TEXT="按钮Button" ID="ID_1766154169" CREATED="1610280519586" MODIFIED="1610280542525"/>
+<node TEXT="图像视图ImageView" ID="ID_1817134092" CREATED="1610280546726" MODIFIED="1610280627240"/>
+<node TEXT="图像按钮ImageButton" ID="ID_1761095523" CREATED="1610280558142" MODIFIED="1610280611760"/>
+</node>
+</node>
+</map>
